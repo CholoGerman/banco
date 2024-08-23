0d12e56e6f2afb0829b2d33364bc715d70697b22
CholoGerman <xrcholo@gmail.com>
2024-08-23 16:26:40 -0300

    asd

---
+++ backend/conexion.php
@@ -0,0 +1,13 @@
+<?php
+function connection()
+{
+    $host = "localhost";
+    $bd = "banco";
+    $usuario = "root";
+    $password = "";
+    $puerto = "3306";
+    $mysqli = new mysqli($host, $usuario, $password, $bd, $puerto);
+    return $mysqli;
+}
+
+?>
