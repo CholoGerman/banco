d1cf4a00fbb26b82bc237bf06f739f2daae97180
CholoGerman <xrcholo@gmail.com>
2024-08-27 17:37:53 -0300

    sdafasd

--- backend/controller/registroController.php
+++ backend/controller/registroController.php
@@ -2,7 +2,7 @@
 
 require_once '../model/registroDAO.php';
 
-$funcion = $_GET['funcion'];
+$funcion = $_POST['funcion'];
 
 switch ($funcion) {
     case "Guardar":
@@ -18,16 +18,16 @@ switch ($funcion) {
 
 
 function GuardarUsuario(){
-    $nombreCompleto = $_GET['nombre_completo'];
-    $numeroCuenta = $_GET['numero_cuenta'];
-    $correo = $_GET['correo_electronico'];
-    $contraseña = $_GET['contraseña'];
+    $nombreCompleto = $_POST['nombre_completo'];
+    $numeroCuenta = $_POST['numero_cuenta'];
+    $correo = $_POST['correo_electronico'];
+    $contraseña = $_POST['contraseña'];
     $resultado = (new usuarios())->GuardarUsuario($nombreCompleto, $numeroCuenta, $correo, $contraseña);
     echo json_encode($resultado);
 }
 
 function EliminarUsuario(){
-    $id_user = $_GET['id_user'];
+    $id_user = $_POST['id_user'];
     $resultado = (new usuarios())->EliminarUsuario($id_user);
     echo json_encode($resultado);
 }
