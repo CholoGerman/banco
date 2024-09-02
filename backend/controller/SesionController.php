fc2d9f4e4becd9505f68fe52865703bee32e9673
CholoGerman <cholorecholo2005@gmail.com>
2024-09-02 19:58:36 -0300

    ssssd

--- backend/controller/SesionController.php
+++ backend/controller/SesionController.php
@@ -1,23 +1,22 @@
 <?php
 require_once '../model/SesionDAO.php';
-function login(){
 
+function login(){
     $correo = $_POST['correo'];
     $contraseña = $_POST['contraseña'];
 
+    $dao = new SesionDAO();
+    $usuario = $dao->getUsuarioCorreo($correo);
 
-if (($)) {
-    session_start();
-    $_SESSION['correo'] = $correo;
-    echo "¡Contraseña correcta!";
-} else {
-    echo "Correo o contraseña incorrectos.";
-    session_destroy();
+    if ($usuario && password_verify($contraseña, $usuario['contraseña'])) {
+        session_start();
+        $_SESSION['correo'] = $correo;
+        echo "¡Contraseña correcta!";
+    } else {
+        echo "Correo o contraseña incorrectos.";
+        session_destroy();
+    }
 }
-
-
-}
-
 
 function logOut(){
     session_start();
@@ -25,5 +24,8 @@ function logOut(){
     header('Location: ../../frontend/page/login.html');
     exit;
 }
+
+
+
 
 ?>
