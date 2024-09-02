61511520917835ebbb3591ad7f94c5bef5f764b3
CholoGerman <cholorecholo2005@gmail.com>
2024-09-02 20:07:12 -0300

    BACKEND

--- backend/controller/cuentasController.php
+++ backend/controller/cuentasController.php
@@ -1,16 +1,6 @@
 <?php
 require_once "../model/cuentas.php";
-/* function cargarSaldo()
-{
 
-
-    $numeroCuenta = $_POST['numero_cuenta'];
-    $monto = $_POST['monto'];
-    $moneda = $_POST['moneda'];
-    $resultado = (new cuentas())->cargarSaldo($numeroCuenta, $monto, $moneda);
-    echo json_encode($resultado);
-}
-*/ 
 function verSaldo()
 {
     $numeroCuenta = $_POST['numero_cuenta'];
